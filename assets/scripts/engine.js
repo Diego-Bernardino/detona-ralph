17f37d9bca76c8e9e464936697efe6f5efff193d
Diego-Bernardino <diegobernardsouza@hotmail.com>
2024-10-19 20:33:59 -0300

    engine completed

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -6,12 +6,32 @@ const state = {
         score: document.querySelector("#score"),
     },
     values: {
-        timerId: null,
         gameVelocity: 1000,
         hitPosition: 0,
         result: 0,
+        currentTime: 60,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
     },
 };
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId)
+        alert(`GAME OVER! O seu resultado foi: ${state.values.result}`);
+    }
+}
+
+function playSound(soundName) {
+    let sound = new Audio(`${soundName}.m4a`);
+    sound.volume = 0.2;
+    sound.play();
+}
 
 function randomSquare() {
     state.view.squares.forEach((square) => {
@@ -24,9 +44,9 @@ function randomSquare() {
     state.values.hitPosition = randomSquare.id
 }
 
-function moveEnemy() {
-    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
-}
+// function moveEnemy() {
+//     state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+// }
 
 function addListenerHitBox() {
     state.view.squares.forEach((square) => {
@@ -35,13 +55,14 @@ function addListenerHitBox() {
                 state.values.result++;
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null
+                playSound("hit");
             }
         });
     });
 }
 
 function main() {
-    moveEnemy();
+    // moveEnemy();
     addListenerHitBox();
 }
 
